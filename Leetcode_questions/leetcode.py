3920e5defc10ac7be98d86995087528e26060990
Thecodemaster-raghav <meraghav342@gmail.com>
2026-06-17 23:11:24 -0600

    A brute force solution to two_sum

--- Leetcode_questions/leetcode.py
+++ Leetcode_questions/leetcode.py
@@ -10,3 +10,18 @@ class Solution(object):
 
 result = Solution().isPalindrome(121)
 print(result)
+
+# Two sum: using lists dicts and loops
+class Solution(object):
+    def two_sum(self, nums, target):
+        self.nums = list(nums)
+        self.target = int(target)
+        for i in range(len(self.nums)):
+            for j in range(len(self.nums)):
+                if i != j:
+                    total = self.nums[i] + self.nums[j]
+                    if total == self.target:
+                        return i, j
+    
+result_twosum = Solution().two_sum([1, 2, 3, 4], 8)
+print(result_twosum)
