8bee2c1e7d031e11ad807b07a09ad666fb05841c
Thecodemaster-raghav <meraghav342@gmail.com>
2026-07-06 11:12:06 -0600

    Longest common prefix: accepted solution, brute force with min-length edge case

--- Leetcode_questions/longest_common_prefix.py
+++ Leetcode_questions/longest_common_prefix.py
@@ -1,16 +1,23 @@
 # find the longest common prefix in a list of strings
 
-# in this problem the brute force solution that i can think of is that we will have a empty list
-# inside of the function and will loop through the list of strings and if the 1st two chars match of th each string 
-# will append it and return that and set a default empty string to return.
+# in this problem the brute force solution that i can think of is that we will have to march through positions and
+# match the chars at those positions with the next chars at same 1st two positions and will need to have a running 
+# empty string that collects the matching chars and if the chars does not match it returns an empty string.
 
 class Solution(object):
     def longestPrefix(self, strs):
-        chars = []
-        for i in strs:
-            if i == strs[0][1]:
-                chars.append(i[0][1])
-        return chars
+        prefix = "" 
+        # now the edge case as the prefix should return the for the shortest word and if not it will give an indexerror
+        shortest = min(strs, key=len)
+        for i in range(len(shortest)):
+            for w in strs:
+                if w[i] != strs[0][i]:
+                    return prefix
+            prefix += shortest[i]
+        return prefix
     
-result = Solution().longestPrefix(["raghav", "ravi", "rajan"])
+result = Solution().longestPrefix(["flower", "flight", "flow"])
 print(result)
+
+# the main crux: for each position check every word; if the instant one disagrees, stop return what we've built;
+# if they all agree keep the chars and move to the next position 
