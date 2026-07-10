5718b02709e548a2d7051147a93a857cc68ace56
Thecodemaster-raghav <meraghav342@gmail.com>
2026-07-09 21:06:41 -0600

    FastAPI mdical API: error handling(404/400) save ptient regardless of reccomendation match

--- Week3/FastAPI_crud.py
+++ Week3/FastAPI_crud.py
@@ -1,10 +1,10 @@
 # a working CRUD API for medicine
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
 patients_data = []
-recommended_meds = {"fever": "paracetamol", "cough": "syrup"}
+recommended_meds = {"fever": "paracetamol", "cough": "syrup", "cold": "tynol"}
 
 # a class called patient to post patients data
 class Patient(BaseModel):
@@ -15,33 +15,40 @@ class Patient(BaseModel):
     
 
 # post data: creating and storing patients data
+# adding a layer of error handling on our api's post, put, del as we want the errors to be said out loud.
+# we will raise the errors not return them in else block of the code.
 @app.post("/patients")
 def create_data(data: Patient):
-    new_data = recommended_meds.get(data.sickness_type)
+    new_data = recommended_meds.get(data.sickness_type) # this throws off the None the .get() on a dict
+    patients_data.append(data)
     if new_data is not None:
-        correct_medication = new_data == data.medication_type
-        patients_data.append(data)
-        return {"patient":data, "correct_medication": correct_medication}
+        correct_meds = new_data == data.medication_type
+        # if None is returned raise the error and if not None work the code.
+        return {"patient_Info": data, "correct_medication": correct_meds}
     else:
-        return {"no reccomendation available for this sickness"}
+        return {"error": "no sickness found"}
 
 @app.get("/patients")
 def read_data():
     return patients_data
 
+# adding the error handling layer 
 
 @app.put("/patients/{patient_id}")
 def update_data(patient_id: int, data: Patient):
-    if patient_id < len(patients_data):
+    if patient_id < len(patients_data): # this is the edge case for the out of bounds error
         patients_data[patient_id] = data
         return data
     else:
-        return {"error": "patient not found"}
+        raise HTTPException(status_code=404, detail="patient not found")
     
 @app.delete("/patients/{patient_id}")
 def delete_data(patient_id: int):
     if patient_id < len(patients_data):
         del patients_data[patient_id]
-        return {"patients data is deletd"}
+        return {"success": "patients data is deleted"}
     else:
-        return {"No patient found"} 
+        raise HTTPException(status_code=404, detail="patient not found")
+    
+# we use 404 error code where theere is some resource is missing and 
+# using 400 on post as the request has unusable data itself.
