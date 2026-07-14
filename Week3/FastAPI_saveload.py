9710b0bc6dbeb30bffc0c273521e4aa762c45940
Thecodemaster-raghav <meraghav342@gmail.com>
2026-07-14 12:28:16 -0600

    WIP: server-assigned song_id

--- Week3/FastAPI_saveload.py
+++ Week3/FastAPI_saveload.py
@@ -10,6 +10,7 @@ class Music(BaseModel):
     song_type : str
     sung_by : str
     spotify_listeners : int
+    song_id : int
 
 def load_data():
     try:
@@ -31,9 +32,15 @@ def read_only():
 
 @app.post("/music")
 def create_data(music_file: Music):
+    biggest = 0
+    for m in musicians_list:
+        if m.song_id > biggest:
+            biggest = m.song_id
+        music_file.song_id = biggest + 1
     musicians_list.append(music_file)
     save_data()
     return music_file
+    
 
 @app.put("/music/{song_id}")
 def update_data(song_id: int, music_file: Music):
@@ -60,3 +67,34 @@ def save_data():
             new_data.append(m.model_dump())
         with open("music.json", "w", encoding="utf-8") as file:
             json.dump(new_data, file, indent=2)
+
+
+# REST VOCAB polish for todays session post work:
+# Resources: what is a resource? Why route /music and not /get_music or /cretae_song?
+
+# Resource is the thing that exists and in our case it is the music files.
+# and the URL is always a Noun never a verb and URL is path that names i want music files
+# the methods like POST, GET, DELETE are the actions. and JSON is the copy of the resource
+# response that travels through post, put request body -> this is called the representation.
+# and representation is the copy of resource. In our case the music files 
+# collection resource in patients API -> /patients
+# the individual resource in patients api -> /patients/{patients_id}
+
+# read or mutate routes in the 2 APIS /patients and /music:
+# # SAFE : An operation is SAFE  that means. It only reads and changes nothing.
+# Idempotent - meaning the final state stays the same even after performing any actions - that is idempotency.
+# POST is the only state which is nt Idempotent in the API.
+# Delete is idempotent in the final state: Meaning the at 1st call the removes the song.
+# but calls from 2-5 hit the else and raie 404, but the system state after call 5
+# equals to system state after 1: THE NUANCE: same state, different responses.
+# one design limitation in the API of why the DELETE is not idempotent is because we check with 
+# position or indexes not with having a value against every position.
+# will build this tomorrow.
+
+# the status codes that we give out as response
+# like 200 means successful response and each number contracts a response attached to it.
+# or someone who has never seen the code behind it.
+# 2xx -> success, 4xx -> clients fault, 5xx -> server fault or internal server error
+# FastAPI sends 200 as the default whenver the function returns normally.
+# error 400 -> when the client fills out wrong information.
+# error 404 -> the resource does not exist
