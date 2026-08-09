671148cb243e775143838a28bff098dde590a348
Thecodemaster-raghav <meraghav342@gmail.com>
2026-08-08 20:44:18 -0600

    timesheet API workers table and get route WIP.

--- timesheet_API/schema.sql
+++ timesheet_API/schema.sql
@@ -1,10 +1,16 @@
---schema for shifts table 
-CREATE TABLE shifts (
-    shift_id serial PRIMARY KEY, --primary key requires values to be both not null and unique
-    worker_id integer NOT NULL REFERENCES workers (worker_id), --in this case we are referencing to workers_id in workers table. 
-    -- which is our foreign key
-    clock_in timestamp with time zone NOT NULL,
-    clock_out timestamp with time zone NULL,
-    CHECK (clock_out > clock_in) --putting a table constraint so that the clock_out time can never be smaller than the clock_in
-    -- time
+-- schema for workers table
+CREATE TABLE workers(
+    name text NOT NULL, --name can not be null
+    worker_id integer NOT NULL PRIMARY KEY,
+    role text NOT NULL DEFAULT 'worker',
+    CHECK (role ())
 );
+
+--schema for shifts table 
+CREATE TABLE shifts(
+    shift_id integer PRIMARY KEY, -- the unique and not null column in the shifts table
+    worker_id integer NOT NULL REFERENCES workers, -- foreign key referenceing workerr_id in workers table
+    clock_in timestamp with time zone NOT NULL, -- DESIGN RULE OF NOT KEEPING CLOCK_IN NULL
+    clock_out timestamp with time zone NULL, -- clock_out gets updated usin PUT route so keeping it null
+    CHECK (clock_out > clock_in) -- clock_in time can never be greater than clocl_out as it would affect the aggregate hours
+); -- and it is a table constraint as it has 2 columns involved 
