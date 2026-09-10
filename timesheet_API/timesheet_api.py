d57700286293f0fd7dd992d92185a23f1023a605
Thecodemaster-raghav <meraghav342@gmail.com>
2026-09-09 20:30:15 -0600

    add admin route for authentication with working 400, 404 and 403 gaurds

--- timesheet_API/timesheet_api.py
+++ timesheet_API/timesheet_api.py
@@ -152,14 +152,31 @@ async def login(user_login: ClientLogin, login_conn=Depends(get_conn)):
 # admin route for updating or changing the clock-in clock-out times for workers
 # query the callers role; 403 if not admin
 @app.patch("/admin/{shift_id}")
-async def entry(shift_id: int, update_entry=Depends(get_conn), tokens=Depends(verify_tokens)):
-    async with update_entry.cursor() as cur:
+async def role(entry: UpdateEntry, shift_id: int, conn=Depends(get_conn), tokens=Depends(verify_tokens)):
+    async with conn.cursor() as cur:
         await cur.execute("SELECT role FROM workers WHERE worker_id=%s", (tokens,))
         rows = await cur.fetchone()
         if rows is None:
-            raise HTTPException(status_code=404, detail="Invalid entry")
+            raise HTTPException(status_code=404, detail="Invalid Entry")
         if rows["role"] != "admin":
-            raise HTTPException(status_code=403, detail="Access Forbidden") 
+            raise HTTPException(status_code=403, detail="Access Forbidden")
+        # for the dynamic UPDATE the in the admin fields
+        pieces = []
+        values = []
+        if entry.clock_in is not None:
+            pieces.append("clock_in=%s") # holds string
+            values.append(entry.clock_in) # holds data
+        if entry.clock_out is not None:
+            pieces.append("clock_out=%s")
+            values.append(entry.clock_out)
+        if not pieces:
+            raise HTTPException(status_code=400, detail="Invalid Entry")
+        # putting the pieces together with join
+        # clock_in , clock_out as strings
+        joined_pieces = (",").join(pieces)
+        await cur.execute(f"UPDATE shifts SET {joined_pieces} WHERE shift_id=%s RETURNING *", values + [shift_id],)
+        admin_rows = await cur.fetchone()
+        return admin_rows 
 
 # POST /shifts route with 2 gaurds where Guard 1 fails when it finds nothing (worker missing). 
 # Guard 2 fails when it finds something (open shift exists).
